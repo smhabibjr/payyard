6bc1cc4a9765d0318fdbe8d4dec39620e66ae962
mhabib <smhabib.abir2030@gmail.com>
2023-09-29 14:45:45 +0200

    add profile avator

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -25,10 +25,15 @@
       bordered
     >
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
+        <q-item-section class="flex flex-center q-mt-md" avatar>
+          <q-avatar size="100px">
+            <img src="https://cdn.quasar.dev/img/avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-label class="flex flex-center" header>
+          Md Habibur Rahman Habib
+          <span>Payyard ID: 0961898</span>
         </q-item-label>
 
         <EssentialLink
@@ -37,6 +42,7 @@
           v-bind="link"
         />
       </q-list>
+
     </q-drawer>
 
     <q-page-container>
