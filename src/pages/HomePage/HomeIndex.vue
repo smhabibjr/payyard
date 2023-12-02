98e0abf0b1f1aaaa97e40e5773598b77d44e6161
mhabib <smhabib.abir2030@gmail.com>
2023-12-02 06:58:19 +0100

    added hero section

--- src/pages/HomePage/HomeIndex.vue
+++ src/pages/HomePage/HomeIndex.vue
@@ -2,11 +2,14 @@
 <template>
  <q-page>
   <Navbar />
+  <Header />
  </q-page>
 </template>
 
 <script setup lang="ts">
 import Navbar from '../../components/HomePage/NavBar.vue';
+import Header from '../../components/HomePage/HeaderHero.vue';
+
 </script>
 
 <style lang="scss" scoped>
