529234744e52b27267403f47832a1cba5831387e
mhabib <smhabib.abir2030@gmail.com>
2023-12-10 17:36:05 +0100

    added transfer calculator

--- src/components/Dashboard/CurrencyConverter.vue
+++ src/components/Dashboard/CurrencyConverter.vue
@@ -7,30 +7,43 @@
             v-model="selectedSendingValue"
             :options="sendingOptions"
             label="You send"
-            @update:model-value="changeSendingCurrency()"
+            @update:model-value="getExchangeRate()"
           />
         </q-card-section>
       </div>
       <div class="col col-xs-12 col-sm-6 col-md-6">
         <q-card-section>
-          <q-select outlined
-          v-model="selectedReceiveValue"
-          :options="recipientOptions"
-          label="Recipient gets"
-          @update:model-value="changeReceiveCurrency()"
-        />
+          <q-select
+            outlined
+            v-model="selectedReceiveValue"
+            :options="recipientOptions"
+            label="Recipient gets"
+            @update:model-value="getExchangeRate()"
+          />
         </q-card-section>
       </div>
     </div>
     <div class="row">
       <div class="col col-xs-12 col-sm-6 col-md-6">
         <q-card-section>
-          <q-input outlined v-model="inputedAmount" label="Amount" />
+          <q-input
+            outlined
+            v-model="inputedAmount"
+            label="Amount"
+            @keyup="updateReceivedAmount"
+          />
         </q-card-section>
       </div>
       <div class="col col-xs-12 col-sm-6 col-md-6">
         <q-card-section>
           <q-input outlined v-model="receivedAmount" label="You will receive" />
+        </q-card-section>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col col-xs-12 col-sm-12 col-md-12 flex justify-center">
+        <q-card-section class="q-ma-none q-pa-none">
+          <p class="q-ma-none q-pa-none text-caption text-weight-light text-dark">Exchange rate {{ selectedSendingValue }} 1 = {{ todayRate }} {{ selectedReceiveValue }}</p>
         </q-card-section>
       </div>
     </div>
@@ -58,25 +71,49 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 const selectedSendingValue = ref('USD');
 const selectedReceiveValue = ref('BDT');
-const inputedAmount = ref('');
-const receivedAmount = ref('');
 
-const sendingOptions = [
-  { label: 'USD', value: 110.09 },
-  { label: 'Euro', value: 118.61 },
-];
+const inputedAmount = ref<number | null>(null);
+const receivedAmount = ref<number | null>(null);
+const todayRate = ref<number | null>(null);
+
+const sendingOptions = ['Euro', 'USD'];
 const recipientOptions = ['BDT', 'PAK'];
 
-function changeSendingCurrency() {
-  console.log(selectedSendingValue.value.value);
+function exchangeRate(fromCurrency, toCurrency) {
+  let receivedExchangeRate = '';
+  const todayRates = {
+    usdToBdt: 110.09,
+    usdToPak: 283.99,
+    euroToBdt: 118.61,
+    euroToPak: 305.96,
+  };
+  if (fromCurrency === 'USD' && toCurrency === 'BDT') {
+    receivedExchangeRate = todayRates.usdToBdt;
+  } else if (fromCurrency === 'USD' && toCurrency === 'PAK') {
+    receivedExchangeRate = todayRates.usdToPak;
+  } else if (fromCurrency === 'Euro' && toCurrency === 'BDT') {
+    receivedExchangeRate = todayRates.euroToBdt;
+  } else if (fromCurrency === 'Euro' && toCurrency === 'PAK') {
+    receivedExchangeRate = todayRates.euroToPak;
+  }
+
+  return receivedExchangeRate;
+}
+const updateReceivedAmount = () => {
+  receivedAmount.value = todayRate.value * inputedAmount.value;
+};
+
+function getExchangeRate() {
+  todayRate.value = exchangeRate(selectedSendingValue.value, selectedReceiveValue.value);
+  updateReceivedAmount();
 }
 
-function changeReceiveCurrency() {
-  console.log(selectedReceiveValue.value);
-}
+onMounted(() => {
+  getExchangeRate();
+});
 
 </script>
