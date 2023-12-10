d0b9940418ea38e1467ab9ea020bfa9a83c69667
mhabib <smhabib.abir2030@gmail.com>
2023-12-10 12:58:43 +0100

    add transfer calculator

---
+++ src/components/Dashboard/CurrencyConverter.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-card>
+    <div class="row">
+      <div class="col col-xs-12 col-sm-6 col-md-6">
+        <q-card-section>
+          <q-select outlined v-model="selectedSendingValue" :options="sendingOptions" label="You send" />
+        </q-card-section>
+      </div>
+      <div class="col col-xs-12 col-sm-6 col-md-6">
+        <q-card-section>
+          <q-select outlined v-model="selectedrecipientValue" :options="recipientOptions" label="Recipient gets" />
+        </q-card-section>
+      </div>
+    </div>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const selectedSendingValue = ref(null);
+const selectedrecipientValue = ref(null);
+
+const sendingOptions = ['Euro', 'USD'];
+const recipientOptions = ['BDT', 'PAK'];
+
+</script>
--- src/pages/dashboardPages/CashWithdrawPage.vue
+++ src/pages/dashboardPages/CashWithdrawPage.vue
@@ -1,25 +1,19 @@
 <template>
   <q-page class="q-pa-sm">
     <PageTitle :title="pageTitle" />
+    <div class="shadow-up-1 q-mt-sm">
+      <currency-converter />
+    </div>
   </q-page>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
+<script setup lang="ts">
+import { ref } from 'vue';
 import PageTitle from 'src/components/PageTitle.vue';
+import CurrencyConverter from 'src/components/Dashboard/CurrencyConverter.vue';
 
-export default defineComponent({
-  name: 'CashWithdraw',
-  components: {
-    PageTitle,
-  },
-  setup() {
-    const pageTitle = ref('Cash Withdraw');
-    return {
-      pageTitle,
-    };
-  },
-});
+const pageTitle = ref('Transfer Calculator');
+
 </script>
 
 <style scoped>
