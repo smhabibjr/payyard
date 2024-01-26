c4521b964cb2a3613af5555b7807679bdffff846
mhabib <smhabib.abir2030@gmail.com>
2024-01-26 14:42:58 +0100

    add new header hero

--- src/pages/HomePage/HomeIndex.vue
+++ src/pages/HomePage/HomeIndex.vue
@@ -1,29 +1,14 @@
 <!-- eslint-disable max-len -->
 <template>
- <q-page class="gradient__bg">
-  <Header />
-<!--   <Brand />
-  <WhatPayyard />
-  <WhyPayyard />
-  <PayyardPosibility />
-  <PayyardCTA />
-  <PayyardBlog />
-  <PayyardFooter /> -->
+ <q-page>
+  <HeaderSection />
+  <ExplainedVideo />
  </q-page>
 </template>
-
 <script setup lang="ts">
-/* import Navbar from '../../components/HomePage/NavBar.vue'; */
-import Header from '../../components/HomePage/HeaderHero.vue';
+import ExplainedVideo from 'src/components/HomePage/ExplainedVideo.vue';
+import HeaderSection from 'src/components/HomePage/HeaderSection.vue';
 
-/* import Navbar from '../../components/HomePage/NavBar.vue';
-import Brand from '../../components/HomePage/Brand/BrandIndex.vue';
-import WhatPayyard from '../../components/HomePage/WhatPayyard.vue';
-import WhyPayyard from '../../components/HomePage/WhyPayyard.vue';
-import PayyardPosibility from '../../components/HomePage/PayyardPosibility.vue';
-import PayyardCTA from '../../components/HomePage/PayyardCTA.vue';
-import PayyardBlog from '../../components/HomePage/PayyardBlog.vue';
-import PayyardFooter from '../../components/HomePage/PayyardFooter.vue'; */
 </script>
 
 <style lang="scss" scoped>
